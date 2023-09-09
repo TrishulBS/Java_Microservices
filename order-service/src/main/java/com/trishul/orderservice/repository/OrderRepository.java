c319393dfa6baf5750c7b2da08cbb7406a4a47f1
TrishulBS <trishulcurtis@gmail.com>
2023-09-09 14:43:10 -0700

    Created Product, Order, and Inventory services

---
+++ order-service/src/main/java/com/trishul/orderservice/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.trishul.orderservice.repository;
+
+import com.trishul.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ order-service/src/main/java/com/trishul/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.trishul.orderservice.service;
+
+import com.trishul.orderservice.dto.OrderLineItemsDto;
+import com.trishul.orderservice.dto.OrderRequest;
+import com.trishul.orderservice.model.Order;
+import com.trishul.orderservice.model.OrderLineItems;
+import com.trishul.orderservice.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
